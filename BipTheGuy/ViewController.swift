afcd5b8fa72d18ee581fce595e61b7c033228206
Lazaro Alvelaez <lazaroalvelaez@bc.edu>
2020-09-19 16:11:10 -0400

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Lazaro Alvelaez on 9/19/20.
+//  Copyright © 2020 Lazaro Alvelaez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
